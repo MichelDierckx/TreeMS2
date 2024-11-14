7be40c577091ad7e370a6efd9365e1aecb949eba
MichelDierckx <michel.dierckx@hotmail.com>
2024-11-14 13:37:12 +0100

    implemented processing pipeline

--- TreeMS2/spectrum/spectrum_processing/pipeline.py
+++ TreeMS2/spectrum/spectrum_processing/pipeline.py
@@ -4,7 +4,13 @@ from typing import Tuple
 import numba as nb
 import spectrum_utils.spectrum as sus
 
-from ...config.spectrum_processing_config import SpectrumProcessingConfig
+from .processors.intensity_filter_processor import IntensityFilterProcessor
+from .processors.intensity_scaling_processor import IntensityScalingProcessor
+from .processors.mz_range_filter_processor import MZRangeFilterProcessor
+from .processors.precursor_peak_remove_processor import PrecursorPeakRemoverProcessor
+from .processors.spectrum_normalizer_processor import SpectrumNormalizerProcessor
+from .processors.spectrum_validator import SpectrumValidator
+from ...config.spectrum_processing_config import SpectrumProcessingConfig, ScalingMethod
 
 
 # Processing pipeline that applies each processor in sequence
@@ -28,25 +34,23 @@ class ProcessingPipelineFactory:
             config.min_mz, config.max_mz, config.fragment_tol
         )
 
-        config.min_mz, config.max_mz, config.fragment_tol
+        validator = SpectrumValidator(min_peaks=config.min_peaks, min_mz_range=config.min_mz_range)
+        processors.append(MZRangeFilterProcessor(mz_min=min_mz, mz_max=max_mz, validator=validator))
 
-        # Dynamically add processors based on config settings
-        if config is not None and "processing" in config:
-            processing_steps = config["processing"]
+        if config.remove_precursor_tol is not None:
+            processors.append(PrecursorPeakRemoverProcessor(remove_precursor_tolerance=config.remove_precursor_tol,
+                                                            validator=validator))
 
-            if processing_steps.get("normalize_intensity", False):
-                processors.append(IntensityNormalizationProcessor())  # Add to the list
+        if config.min_intensity is not None or config.max_peaks_used is not None:
+            processors.append(
+                IntensityFilterProcessor(min_intensity=config.min_intensity, max_peaks_used=config.max_peaks_used,
+                                         validator=validator))
 
-            if "filter_low_intensity" in processing_steps:
-                threshold = processing_steps["filter_low_intensity"].get("threshold", 0.1)
-                processors.append(LowIntensityFilterProcessor(threshold))
+        if not config.scaling != ScalingMethod.OFF:
+            processors.append(IntensityScalingProcessor(scaling=config.scaling, max_rank=config.max_peaks_used))
 
-            # Add more processors here as needed based on the config options
-            # For example:
-            # if processing_steps.get("other_processing", False):
-            #     processors.append(OtherProcessor(...))
+        processors.append(SpectrumNormalizerProcessor())
 
-        # Return the constructed pipeline with all relevant processors
         return SpectrumProcessingPipeline(processors)
 
 
--- TreeMS2/spectrum/spectrum_processing/processors/intensity_scaling_processor.py
+++ TreeMS2/spectrum/spectrum_processing/processors/intensity_scaling_processor.py
@@ -1,15 +1,17 @@
-from typing import Optional
+from typing import Optional, Literal
 
 import spectrum_utils.spectrum as sus
 
 from ..spectrum_processor import SpectrumProcessor
+from ....config.spectrum_processing_config import ScalingMethod
 
 
 class IntensityScalingProcessor(SpectrumProcessor):
-    def __init__(self, scaling: Optional[str], max_rank: Optional[int]):
+    def __init__(self, scaling: Literal[ScalingMethod.ROOT, ScalingMethod.LOG, ScalingMethod.RANK],
+                 max_rank: Optional[int]):
         super().__init__()
         self.scaling = scaling
         self.max_rank = max_rank
 
     def change(self, spectrum: sus.MsmsSpectrum) -> sus.MsmsSpectrum:
-        return spectrum.scale_intensity(self.scaling, max_rank=self.max_rank)
+        return spectrum.scale_intensity(self.scaling.value, max_rank=self.max_rank)
