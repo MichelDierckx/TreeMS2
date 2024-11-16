c85a146024be6ceda264a5cfc9d9c341a38cd454
MichelDierckx <michel.dierckx@hotmail.com>
2024-11-16 14:35:39 +0100

    added TreeMS2.run()

--- TreeMS2/main.py
+++ TreeMS2/main.py
@@ -1,10 +1,12 @@
 from typing import Union, List
 
 from .config.config_factory import ConfigFactory
+from .tree_ms2 import TreeMS2
 
 
 def main(args: Union[str, List[str]] = None) -> int:
     config_factory = ConfigFactory()
     config_factory.parse(args)  # Parse arguments from config file or command-line
-
+    app = TreeMS2(config_factory)
+    app.run()
     return 0
